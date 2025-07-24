cc53171875cbc9145883a8cd2a928e036d381dfd
haruhi <baijiu1100@hotmail.com>
2025-07-24 20:33:32 +0800

    Delete cmake-build-debug directory

--- cmake-build-debug/src/common/libconfig/tinytest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/haruhi/ClionProjects/mysql-replicater/src/common/libconfig/tinytest
-# Build directory: /Users/haruhi/ClionProjects/mysql-replicater/cmake-build-debug/src/common/libconfig/tinytest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
